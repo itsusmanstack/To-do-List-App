4dc85cdcc6635a7962865e72e11ef75d674fade0
Usman <usmanbuttt888@gmail.com>
2026-07-24 09:15:36 +0500

    First commit

---
+++ To-Do-List-App/script.js
@@ -0,0 +1,33 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const task = input.value.trim();
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = task;
+
+    span.onclick = function () {
+        span.classList.toggle("completed");
+    };
+
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.className = "delete";
+
+    delBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(span);
+    li.appendChild(delBtn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
